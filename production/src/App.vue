f599467f05a3789988faaa51fe74115724fa8af3
hahnah <superhahnah@gmail.com>
2019-02-23 18:10:05 +0900

    Implement Card component

--- production/src/App.vue
+++ production/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Card number=1 />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Card from './components/Card.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Card,
   },
 })
 export default class App extends Vue {}
---
+++ production/src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <h1>{{ number }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Card extends Vue {
+  @Prop() private number!: number;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>

f6293fbfd48112e07fa963e04ce850df40093495
hahnah <superhahnah@gmail.com>
2019-02-23 17:25:09 +0900

    Implement state

--- production/src/store.ts
+++ production/src/store.ts
@@ -3,9 +3,29 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+export enum Player {
+  player1,
+  player2,
+}
+
+export enum Scene {
+  preparing,
+  playing,
+  finish,
+}
+
 export default new Vuex.Store({
   state: {
-
+    personalCardsFieldOfPlayer1: null,
+    personalCardsFieldOfPlayer2: null,
+    commonCardsField: null,
+    gainCardsFieldOfPlayer1: null,
+    gainCardsFieldOfPlayer2: null,
+    turnPlayer: Player.player1,
+    counter: 0,
+    openedCards: 0,
+    isPrivilegeAvailable: true,
+    scene: Scene.preparing,
   },
   mutations: {
 

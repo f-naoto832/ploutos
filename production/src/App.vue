34380f9c76b2a120e5c45ec7007c5cbe346cf5bd
hahnah <superhahnah@gmail.com>
2019-03-01 14:29:18 +0900

    Add an access modifier to remove compiler warning

--- production/src/App.vue
+++ production/src/App.vue
@@ -20,7 +20,7 @@ import Card from './components/Card.vue';
 })
 export default class App extends Vue {
   // 動作確認用関数
-  onClickNext() {
+  private onClickNext() {
     store.commit('incrementTurnCount');
   }
 }
